edee2b07549633a3b9855efe1ad7fa25d525e7a5
StarHE99 <starhe@ucsc.edu>
2021-04-11 19:32:57 -0700

    trying to load explosion?????

--- Rocket Patrol Remake/src/scenes/Play.js
+++ Rocket Patrol Remake/src/scenes/Play.js
@@ -7,6 +7,8 @@ class Play extends Phaser.Scene {
         this.load.image('starfield', 'assets/starfield.png');
         this.load.image('rocket', 'assets/rocket.png');
         this.load.image('spaceship', 'assets/spaceship.png');
+        // load spritesheet
+        this.load.spritesheet('explosion', 'assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
     }
 
     create() {
@@ -62,6 +64,8 @@ class Play extends Phaser.Scene {
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);      
+        // animation config
+        this.anims.create({key: 'explode', frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 9, first: 0}),frameRate: 30});    
     }
 
     update() {
@@ -71,9 +75,19 @@ class Play extends Phaser.Scene {
         this.ship2.update();
         this.ship3.update();
         
-        this.checkCollision(this.p1Rocket, this.ship1);
-        this.checkCollision(this.p1Rocket, this.ship2);
-        this.checkCollision(this.p1Rocket, this.ship3);
+        // check collisions
+        if(this.checkCollision(this.p1Rocket, this.ship3)) {
+            this.p1Rocket.reset();
+            this.shipExplode(this.ship3);   
+        }
+        if (this.checkCollision(this.p1Rocket, this.ship2)) {
+            this.p1Rocket.reset();
+            this.shipExplode(this.ship2);
+        }
+        if (this.checkCollision(this.p1Rocket, this.ship1)) {
+            this.p1Rocket.reset();
+            this.shipExplode(this.ship1);
+        }
 
     }
 
@@ -87,4 +101,16 @@ class Play extends Phaser.Scene {
                 ship.reset();
         }
     }
+    shipExplode(ship) {
+        // temporarily hide ship
+        ship.alpha = 0;
+        // create explosion sprite at ship's position
+        let boom = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0, 0);
+        boom.anims.play('explode');             // play explode animation
+        boom.on('animationcomplete', () => {    // callback after anim completes
+          ship.reset();                         // reset ship position
+          ship.alpha = 1;                       // make ship visible again
+          boom.destroy();                       // remove explosion sprite
+        });       
+      }
 }
